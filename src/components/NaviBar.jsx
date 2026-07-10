07ca3d062530956347d8a3eacd2436ef1158aaca
aryapradeep1 <aaryapradeep04@gmail.com>
2026-07-10 21:21:09 +0530

    navigation page completed

---
+++ src/components/NaviBar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const NaviBar = () => {
+  return (
+    <div>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Pet Adoption</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+       <a class="nav-link" href="#">Add Pets</a>
+        <a class="nav-link" href="#">Search Pets</a>
+        <a class="nav-link" href="#">Delete Pets</a>
+       <a class="nav-link" href="#">View Pets</a>
+       <a class="nav-link" href="#">About Us</a>
+      </div>
+    </div>
+  </div>
+</nav>
+
+
+
+    </div>
+  )
+}
+
+export default NaviBar
